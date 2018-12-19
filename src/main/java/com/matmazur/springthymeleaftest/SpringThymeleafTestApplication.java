c2aa9c9de1f2c14d76ad501b1f88aa1070098a32
Bingo Bobo <matmazur90@gmail.com>
2018-12-19 13:59:33 +0100

    initial commit

---
+++ src/main/java/com/matmazur/springthymeleaftest/SpringThymeleafTestApplication.java
@@ -0,0 +1,14 @@
+package com.matmazur.springthymeleaftest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafTestApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/matmazur/springthymeleaftest/SpringThymeleafTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.matmazur.springthymeleaftest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringThymeleafTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
