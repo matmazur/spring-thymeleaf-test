b68d59ebb613ce4614d83b7bd6a8c5e241f05618
Bingo Bobo <matmazur90@gmail.com>
2018-12-21 15:31:55 +0100

    everything works well, problem with using polish letters in messages strictly. question marks instead. no clue on solution

--- src/main/java/com/matmazur/springthymeleaftest/SpringThymeleafTestApplication.java
+++ src/main/java/com/matmazur/springthymeleaftest/SpringThymeleafTestApplication.java
@@ -3,11 +3,16 @@ package com.matmazur.springthymeleaftest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class SpringThymeleafTestApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringThymeleafTestApplication.class, args);
+        Locale.setDefault(new Locale("pl", "PL"));
+
     }
 
 }
--- src/main/java/com/matmazur/springthymeleaftest/controllers/HomeController.java
+++ src/main/java/com/matmazur/springthymeleaftest/controllers/HomeController.java
@@ -3,10 +3,7 @@ package com.matmazur.springthymeleaftest.controllers;
 import com.matmazur.springthymeleaftest.model.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,6 +11,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @Controller
@@ -22,11 +20,14 @@ public class HomeController {
     private List<Article> allArticles = new ArrayList<>();
 
     @GetMapping("/")
-    public String home(ModelMap modelMap) {
+    public String home(ModelMap modelMap, @RequestHeader("Accept-Language") String encoding ) {
 
         modelMap.put("hello", "Hello Thyme!");
         modelMap.put("article", new Article());
         modelMap.put("allArticles", allArticles);
+
+        System.out.println(encoding);
+
 
         return "home";
     }
